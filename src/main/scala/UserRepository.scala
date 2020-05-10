54453045daa2ca49aab73e2b5142e22bdc5a9508
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2020-05-10 13:56:44 +0200

    Comment code

--- src/main/scala/UserRepository.scala
+++ src/main/scala/UserRepository.scala
@@ -4,9 +4,7 @@ import com.datastax.driver.core.Row
 import com.datastax.driver.core.Session
 
 class UserRepository(session: Session) {
-  /**
-   * Create keyspace uprofile in cassandra DB
-   */
+  // Create keyspace uprofile in cassandra DB
   def createKeyspace(): Unit = {
     val query = "CREATE KEYSPACE IF NOT EXISTS uprofile WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1 }"
     session.execute(query)
@@ -14,9 +12,7 @@ class UserRepository(session: Session) {
     println("Created keyspace 'uprofile'")
   }
 
-  /**
-   * Create user table in cassandra DB
-   */
+  // Create user table in cassandra DB
   def createTable(): Unit = {
     val query = "CREATE TABLE IF NOT EXISTS uprofile.user (user_id int PRIMARY KEY, user_name text, user_bcity text)"
     session.execute(query)
@@ -24,9 +20,7 @@ class UserRepository(session: Session) {
     println("Created table 'user'")
   }
 
-  /**
-   * Select all rows from user table
-   */
+  // Select all rows from user table
   def selectAllUsers(): Unit = {
     val query = "SELECT * FROM uprofile.user"
     val rows = session.execute(query).all
@@ -34,11 +28,7 @@ class UserRepository(session: Session) {
     rows.forEach(row => println(row.getString("user_name")))
   }
 
-  /**
-   * Select a row from user table
-   *
-   *
-   */
+  // Select a row from user table
   def selectUser(id: Int): Unit = {
     val query = "SELECT * FROM uprofile.user where user_id = 3"
     val row = session.execute(query).one
@@ -46,31 +36,19 @@ class UserRepository(session: Session) {
     println("Obtained row: {} | {} | {} ", row.getInt("user_id"), row.getString("user_name"), row.getString("user_bcity"))
   }
 
-  /**
-   * Delete user table.
-   */
+  // Delete user table.
   def deleteTable(): Unit = {
     val query = "DROP TABLE IF EXISTS uprofile.user"
     session.execute(query)
   }
 
-  /**
-   * Insert a row into user table
-   *
-   * @param id   user_id
-   * @param name user_name
-   * @param city user_bcity
-   */
+  // Insert a row into user table
   def insertUser(statement: PreparedStatement, id: Int, name: String, city: String): Unit = {
     val boundStatement = new BoundStatement(statement)
     session.execute(boundStatement.bind(id, name, city))
   }
 
-  /**
-   * Create a PrepareStatement to insert a row to user table
-   *
-   * @return PreparedStatement
-   */
+  // Create a PrepareStatement to insert a row to user table
   def prepareInsertStatement(): PreparedStatement = {
     val insertStatement = "INSERT INTO  uprofile.user (user_id, user_name , user_bcity) VALUES (?,?,?)"
     session.prepare(insertStatement)
